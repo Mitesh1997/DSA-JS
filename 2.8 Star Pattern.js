07254ab4b1b156563b89a5515ab2d68612ef8c22
mitesh <miteshsarjare@gmail.com>
2026-01-22 15:45:06 +0530

    count Digits

---
+++ 2.8 Star Pattern.js
@@ -0,0 +1,18 @@
+// let star=5
+// for(let i=0;i<=5;i++){
+//     // console.log(star.repeat(i));
+//     let row=" ";
+//     for(let j=0;j<i;j++){
+//         row=row+"*"
+//     }
+//     console.log(re);
+    
+// }
+
+
+
+const add=(a,b)=>{
+ a+b
+}
+
+console.log(add(2,3));
